4358ece8a6e1079bdf40ac4e86363f87a67cdd60
Artem <7555524@mail.ru>
2018-01-23 15:20:50 +0300

    UserService done

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -42,25 +42,83 @@ public class UserService extends Util implements UserDAO {
         try {
             statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery(sql);
+            while (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getInt("ID"));
+                user.setName(resultSet.getString("NAME"));
+                user.setOs(resultSet.getString("OS"));
+                user.setBirthday(resultSet.getDate("BIRTHDAY"));
+                userList.add(user);
+            }
         } catch (SQLException e) {
+            CustomLog.log("Get all contacts error", e);
+            CustomLog.log("Get all contacts error", e);
             e.printStackTrace();
+        } finally {
+            closeConnections(statement, connection);
         }
         return userList;
     }
 
     @Override
     public User getById(int id) {
-        return null;
+        PreparedStatement ps = null;
+        String sql = "SELECT ID, NAME, OS, BIRTHDAY FROM USER WHERE ID=?";
+        User user = new User();
+        try {
+            ps = connection.prepareStatement(sql);
+            ps.setInt(1, id);
+
+            ResultSet resultSet = ps.executeQuery();
+
+            user.setId(resultSet.getInt("ID"));
+            user.setName(resultSet.getString("NAME"));
+            user.setOs(resultSet.getString("OS"));
+            user.setBirthday(resultSet.getDate("BIRTHDAY"));
+        } catch (SQLException e) {
+            CustomLog.log("Get all contacts error", e);
+            e.printStackTrace();
+        } finally {
+            closeConnections(ps, connection);
+        }
+        return user;
     }
 
     @Override
     public void update(User user) {
+        PreparedStatement ps = null;
+        String sql = "UPDATE USER SET NAME=?, OS=?, BIRTHDAY=? WHERE ID=?";
+        try{
+            ps = connection.prepareStatement(sql);
 
+            ps.setString(1, user.getName());
+            ps.setString(2, user.getOs());
+            ps.setDate(3, user.getBirthday());
+            ps.setInt(4, user.getId());
+
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            CustomLog.log("Get all contacts error", e);
+            e.printStackTrace();
+        } finally {
+            closeConnections(ps, connection);
+        }
     }
 
     @Override
     public void delete(User user) {
-
+        PreparedStatement ps = null;
+        String sql = "DELETE FROM USER WHERE ID=?";
+        try{
+            ps = connection.prepareStatement(sql);
+            ps.setInt(1, user.getId());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            CustomLog.log("Get all contacts error", e);
+            e.printStackTrace();
+        } finally {
+            closeConnections(ps, connection);
+        }
     }
 
     public void closeConnections(PreparedStatement ps, Connection connection) {
